90df4bb9304614a2576bd6bc6717eeafad9fa347
haoyuli <1065618302@qq.com>
2019-05-23 22:30:18 +0800

    添加注释

--- controllers/base.go
+++ controllers/base.go
@@ -7,8 +7,10 @@ import (
 	"lcc/blog/models"
 	"ticket/syserror"
 )
+
 //定义保存用户session的常量
-const SESSION_USER_KEY  = "SESSION_USER_KEY"
+const SESSION_USER_KEY = "SESSION_USER_KEY"
+
 type BaseController struct {
 	beego.Controller
 	//用来保存登录的用户
@@ -17,9 +19,10 @@ type BaseController struct {
 	IsLogin bool
 }
 
-func (this *BaseController) Prepare(){
+func (this *BaseController) Prepare() {
 	//拿到当前访问的路径，比较是否为当前页面（用于添加下划线）
 	this.Data["Path"] = this.Ctx.Request.RequestURI
+	//得到当前登录的session
 	u, ok := this.GetSession(SESSION_USER_KEY).(models.User)
 	//进行类型判断 如果用户是能得到的
 	if ok {
@@ -33,17 +36,16 @@ func (this *BaseController) Prepare(){
 
 }
 
-
 //自定义一个错误
-func (this *BaseController)Abort500(err error){
-	this.Data["error"]=err
+func (this *BaseController) Abort500(err error) {
+	this.Data["error"] = err
 	this.Abort("500")
 }
 
 //确保输入的值不为空
-func (this *BaseController) GetMustString(key,msg string) string {
-	s :=this.GetString(key,"")
-	if len(s)==0 {
+func (this *BaseController) GetMustString(key, msg string) string {
+	s := this.GetString(key, "")
+	if len(s) == 0 {
 		this.Abort500(errors.New(msg))
 	}
 	return s
@@ -57,18 +59,18 @@ func (this *BaseController) MustLogin() {
 }
 
 //定义返回的数据类型
-type H map[string] interface{}
+type H map[string]interface{}
 
 func (this *BaseController) JsonOk(msg string) {
 	this.Data["json"] = H{
-		"code":0,
-		"msg":msg,
+		"code": 0,
+		"msg":  msg,
 	}
 	this.ServeJSON()
 }
 
-func (this *BaseController) JsonOkH(msg string,data H) {
-	if data==nil {
+func (this *BaseController) JsonOkH(msg string, data H) {
+	if data == nil {
 		data = H{}
 	}
 	data["code"] = 0
@@ -77,20 +79,20 @@ func (this *BaseController) JsonOkH(msg string,data H) {
 	this.ServeJSON()
 }
 
-func (this *BaseController) JsonOkData(msg string,data H) {
+func (this *BaseController) JsonOkData(msg string, data H) {
 	this.Data["json"] = H{
-		"code":0,
-		"msg":msg,
-		"data":data,
+		"code": 0,
+		"msg":  msg,
+		"data": data,
 	}
 	this.ServeJSON()
 }
 
-
+//生成UUID主键
 func (this *BaseController) UUID() string {
-	u,err:=uuid.NewV4()
-	if err!=nil {
-		this.Abort500(syserror.New("系统错误",err))
+	u, err := uuid.NewV4()
+	if err != nil {
+		this.Abort500(syserror.New("系统错误", err))
 	}
 	return u.String()
 }
